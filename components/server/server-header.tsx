4369e029d26e5f768afdc39f35f3ae0c42aa59e3
yoshinao <yoshinao@fit.cvut.cz>
2023-08-31 22:23:51 +0900

    feat: manage users

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -78,7 +78,8 @@ export const ServerHeader = ({
 
         {isAdmin && (
           <DropdownMenuItem
-          className=" px-3 py-2 text-sm cursor-pointer"
+          onClick={() => onOpen("members", { server })}
+          className="px-3 py-2 text-sm cursor-pointer"
           >
             Manage Members
             <Users className="h-4 w-4 ml-auto"/>
